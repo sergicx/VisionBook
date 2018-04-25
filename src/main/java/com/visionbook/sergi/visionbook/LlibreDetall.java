12688e394eda36b84220aa235664acf238e72b47
Sergi Cañuelo <sergicx96@gmail.com>
2018-04-25 17:08:25 +0000

    Update LlibreDetall.java

--- src/main/java/com/visionbook/sergi/visionbook/LlibreDetall.java
+++ src/main/java/com/visionbook/sergi/visionbook/LlibreDetall.java
@@ -1,13 +1,23 @@
 package com.visionbook.sergi.visionbook;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class LlibreDetall extends AppCompatActivity {
 
@@ -43,7 +53,45 @@ public class LlibreDetall extends AppCompatActivity {
         }
         tvAutor.setText(autorsBuilder.toString());
         tvResum.setText(llibre.getDescripcio());
+        try {
+            Bitmap portada = new DescarregarPortada().execute(llibre.getUrlImatge()).get();
+            ivPortada.setImageBitmap(portada);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    private class DescarregarPortada extends AsyncTask<String, Void, Bitmap> {
+        private String TAG = "DownloadImage";
+        private Bitmap downloadImageBitmap(String sUrl) {
+            Bitmap bitmap = null;
+            try {
+                InputStream inputStream = new URL(sUrl).openStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+                inputStream.close();
+            } catch (Exception e) {
+                Log.d(TAG, "Exception 1, Something went wrong!");
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPreExecute(){
+            ProgressDialog dialog = ProgressDialog.show(LlibreDetall.this, "Loading...", "Please wait...", true);
+            dialog.show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            return downloadImageBitmap(params[0]);
+        }
+
+    }
+
+
 
 }
